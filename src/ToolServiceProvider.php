86f0d7e4d0dd39d1dccd9c23895d65fc44696ef6
Norman Huth <norman@huth.it>
2024-03-10 16:10:47 +0100

    Initial commit

---
+++ src/ToolServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace NormanHuth\NovaResourceCommentable;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
+
+class ToolServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+}
